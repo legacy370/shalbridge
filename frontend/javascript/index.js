8daf3e3ecdaa823ecb5d9810b43909b0761f08bc
Norman Snyder <legacy370@gmail.com>
2026-04-27 17:08:40 -0400

    Added bridgetown search

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -3,5 +3,6 @@ import "$styles/syntax-highlighting.css"
 
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
+import "bridgetown-quick-search"
 
 console.info("Bridgetown is loaded!")
